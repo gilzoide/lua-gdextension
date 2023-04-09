3bdc9b1d3f581e7b1a2df3b6e182e5e6251157b6
gilzoide <gilzoide@gmail.com>
2023-04-08 22:27:16 -0300

    Rename lua libs enum to use `LUA_` prefix

--- src/LuaState.cpp
+++ src/LuaState.cpp
@@ -37,19 +37,19 @@ LuaState::LuaState() : lua_state(sol::default_at_panic, lua_alloc) {
 
 void LuaState::_bind_methods() {
 	// Library enum
-	BIND_BITFIELD_FLAG(BASE);
-	BIND_BITFIELD_FLAG(PACKAGE);
-	BIND_BITFIELD_FLAG(COROUTINE);
-	BIND_BITFIELD_FLAG(STRING);
-	BIND_BITFIELD_FLAG(OS);
-	BIND_BITFIELD_FLAG(MATH);
-	BIND_BITFIELD_FLAG(TABLE);
-	BIND_BITFIELD_FLAG(DEBUG);
-	BIND_BITFIELD_FLAG(BIT32);
-	BIND_BITFIELD_FLAG(IO);
-	BIND_BITFIELD_FLAG(FFI);
-	BIND_BITFIELD_FLAG(JIT);
-	BIND_BITFIELD_FLAG(UTF8);
+	BIND_BITFIELD_FLAG(LUA_BASE);
+	BIND_BITFIELD_FLAG(LUA_PACKAGE);
+	BIND_BITFIELD_FLAG(LUA_COROUTINE);
+	BIND_BITFIELD_FLAG(LUA_STRING);
+	BIND_BITFIELD_FLAG(LUA_OS);
+	BIND_BITFIELD_FLAG(LUA_MATH);
+	BIND_BITFIELD_FLAG(LUA_TABLE);
+	BIND_BITFIELD_FLAG(LUA_DEBUG);
+	BIND_BITFIELD_FLAG(LUA_BIT32);
+	BIND_BITFIELD_FLAG(LUA_IO);
+	BIND_BITFIELD_FLAG(LUA_FFI);
+	BIND_BITFIELD_FLAG(LUA_JIT);
+	BIND_BITFIELD_FLAG(LUA_UTF8);
 	BIND_BITFIELD_FLAG(LUA);
 
 	// Methods
@@ -69,43 +69,43 @@ void LuaState::open_libraries(BitField<Library> libraries) {
 		lua_state.open_libraries();
 	}
 	else {
-		if (libraries.has_flag(BASE)) {
+		if (libraries.has_flag(LUA_BASE)) {
 			lua_state.open_libraries(sol::lib::base);
 		}
-		if (libraries.has_flag(PACKAGE)) {
+		if (libraries.has_flag(LUA_PACKAGE)) {
 			lua_state.open_libraries(sol::lib::package);
 		}
-		if (libraries.has_flag(COROUTINE)) {
+		if (libraries.has_flag(LUA_COROUTINE)) {
 			lua_state.open_libraries(sol::lib::coroutine);
 		}
-		if (libraries.has_flag(STRING)) {
+		if (libraries.has_flag(LUA_STRING)) {
 			lua_state.open_libraries(sol::lib::string);
 		}
-		if (libraries.has_flag(OS)) {
+		if (libraries.has_flag(LUA_OS)) {
 			lua_state.open_libraries(sol::lib::os);
 		}
-		if (libraries.has_flag(MATH)) {
+		if (libraries.has_flag(LUA_MATH)) {
 			lua_state.open_libraries(sol::lib::math);
 		}
-		if (libraries.has_flag(TABLE)) {
+		if (libraries.has_flag(LUA_TABLE)) {
 			lua_state.open_libraries(sol::lib::table);
 		}
-		if (libraries.has_flag(DEBUG)) {
+		if (libraries.has_flag(LUA_DEBUG)) {
 			lua_state.open_libraries(sol::lib::debug);
 		}
-		if (libraries.has_flag(BIT32)) {
+		if (libraries.has_flag(LUA_BIT32)) {
 			lua_state.open_libraries(sol::lib::bit32);
 		}
-		if (libraries.has_flag(IO)) {
+		if (libraries.has_flag(LUA_IO)) {
 			lua_state.open_libraries(sol::lib::io);
 		}
-		if (libraries.has_flag(FFI)) {
+		if (libraries.has_flag(LUA_FFI)) {
 			lua_state.open_libraries(sol::lib::ffi);
 		}
-		if (libraries.has_flag(JIT)) {
+		if (libraries.has_flag(LUA_JIT)) {
 			lua_state.open_libraries(sol::lib::jit);
 		}
-		if (libraries.has_flag(UTF8)) {
+		if (libraries.has_flag(LUA_UTF8)) {
 			lua_state.open_libraries(sol::lib::utf8);
 		}
 	}
--- src/LuaState.hpp
+++ src/LuaState.hpp
@@ -38,34 +38,34 @@ public:
 	enum Library {
 		// ----- Lua/LuaJIT ----
 		// print, assert, and other base functions
-		BASE = 1 << 0,
+		LUA_BASE = 1 << 0,
 		// require and other package functions
-		PACKAGE = 1 << 1,
+		LUA_PACKAGE = 1 << 1,
 		// coroutine functions and utilities
-		COROUTINE = 1 << 2,
+		LUA_COROUTINE = 1 << 2,
 		// string library
-		STRING = 1 << 3,
+		LUA_STRING = 1 << 3,
 		// functionality from the OS
-		OS = 1 << 4,
+		LUA_OS = 1 << 4,
 		// all things math
-		MATH = 1 << 5,
+		LUA_MATH = 1 << 5,
 		// the table manipulator and observer functions
-		TABLE = 1 << 6,
+		LUA_TABLE = 1 << 6,
 		// the debug library
-		DEBUG = 1 << 7,
+		LUA_DEBUG = 1 << 7,
 		// the bit library: different based on which you're using
-		BIT32 = 1 << 8,
+		LUA_BIT32 = 1 << 8,
 		// input/output library
-		IO = 1 << 9,
+		LUA_IO = 1 << 9,
 		// LuaJIT only
-		FFI = 1 << 10,
+		LUA_FFI = 1 << 10,
 		// LuaJIT only
-		JIT = 1 << 11,
+		LUA_JIT = 1 << 11,
 		// library for handling utf8: new to Lua
-		UTF8 = 1 << 12,
+		LUA_UTF8 = 1 << 12,
 
 		// all of the above
-		LUA = BASE | PACKAGE | COROUTINE | STRING | OS | MATH | TABLE | DEBUG | BIT32 | IO | FFI | JIT | UTF8,
+		LUA = LUA_BASE | LUA_PACKAGE | LUA_COROUTINE | LUA_STRING | LUA_OS | LUA_MATH | LUA_TABLE | LUA_DEBUG | LUA_BIT32 | LUA_IO | LUA_FFI | LUA_JIT | LUA_UTF8,
 
 		// ----- Godot ----
 	};
