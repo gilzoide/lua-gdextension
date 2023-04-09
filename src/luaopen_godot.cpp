068bacc7f8615926964410ab1b4f5e0fbebcdbb8
gilzoide <gilzoide@gmail.com>
2023-04-08 23:23:33 -0300

    Add bindings for Vector3

--- src/luaopen_godot.cpp
+++ src/luaopen_godot.cpp
@@ -24,10 +24,13 @@
 
 #include "godot_utils.hpp"
 
+#include <godot_cpp/variant/basis.hpp>
 #include <godot_cpp/variant/packed_byte_array.hpp>
 #include <godot_cpp/variant/string.hpp>
 #include <godot_cpp/variant/vector2.hpp>
 #include <godot_cpp/variant/vector2i.hpp>
+#include <godot_cpp/variant/vector3.hpp>
+#include <godot_cpp/variant/vector3i.hpp>
 #include <sol/sol.hpp>
 
 using namespace godot;
@@ -47,31 +50,43 @@ sol::object to_lua_string(sol::this_state L, const T& value) {
 	return sol::object(L, sol::in_place, to_string_view(bytes));
 }
 
+template<typename TVector>
+char vector_min_axis(const TVector& v) {
+	const char *vector_axes = "xyzw";
+	return vector_axes[v.min_axis_index()];
+}
+template<typename TVector>
+char vector_max_axis(const TVector& v) {
+	const char *vector_axes = "xyzw";
+	return vector_axes[v.max_axis_index()];
+}
+
 int luaopen_godot_math(lua_State *L) {
 	sol::state_view state = L;
 
 	state.new_usertype<Vector2>(
 		"Vector2",
 		// constructors
-		sol::call_construction(),
-		sol::constructors<Vector2(), Vector2(real_t, real_t)>(),
+		sol::call_constructor, sol::constructors<Vector2(), Vector2(real_t, real_t)>(),
 		// properties
-		"x", &Vector2::x,
+		"x", &Vector2::x,  // (x, y)
 		"y", &Vector2::y,
-		"width", &Vector2::x,
+		"width", &Vector2::x,  // (width, height)
 		"height", &Vector2::y,
-		"r", &Vector2::x,
+		"r", &Vector2::x,  // (r, g)
 		"g", &Vector2::y,
-		"s", &Vector2::x,
+		"s", &Vector2::x,  // (s, t)
 		"t", &Vector2::y,
-		"u", &Vector2::x,
+		"u", &Vector2::x,  // (u, v)
 		"v", &Vector2::y,
 		// constants
 		"AXIS_X", sol::var(Vector2::AXIS_X),
 		"AXIS_Y", sol::var(Vector2::AXIS_Y),
 		// methods
 		"min_axis_index", &Vector2::min_axis_index,
+		"min_axis", &vector_min_axis<Vector2>,
 		"max_axis_index", &Vector2::max_axis_index,
+		"max_axis", &vector_max_axis<Vector2>,
 		"normalize", &Vector2::normalize,
 		"normalized", &Vector2::normalized,
 		"is_normalized", &Vector2::is_normalized,
@@ -130,6 +145,96 @@ int luaopen_godot_math(lua_State *L) {
 			sol::resolve<Vector2(const real_t&) const>(&Vector2::operator /)
 		),
 		sol::meta_function::to_string, &to_lua_string<Vector2>
+	);
+
+	state.new_usertype<Vector3>(
+		"Vector3",
+		// constructors
+		sol::call_constructor, sol::constructors<Vector3(), Vector3(real_t, real_t, real_t)>(),
+		// properties
+		"x", &Vector3::x,  // (x, y, z)
+		"y", &Vector3::y,
+		"z", &Vector3::z,
+		"width", &Vector3::x,  // (width, height, depth)
+		"height", &Vector3::y,
+		"depth", &Vector3::z,
+		"r", &Vector3::x,  // (r, g, b)
+		"g", &Vector3::y,
+		"b", &Vector3::z,
+		"s", &Vector3::x,  // (s, t, p)
+		"t", &Vector3::y,
+		"p", &Vector3::z,
+		"u", &Vector3::x,  // (u, v)
+		"v", &Vector3::y,
+		// constants
+		"AXIS_X", sol::var(Vector3::AXIS_X),
+		"AXIS_Y", sol::var(Vector3::AXIS_Y),
+		"AXIS_Z", sol::var(Vector3::AXIS_Z),
+		// methods
+		"min_axis_index", &Vector3::min_axis_index,
+		"min_axis", &vector_min_axis<Vector3>,
+		"max_axis_index", &Vector3::max_axis_index,
+		"max_axis", &vector_max_axis<Vector3>,
+		"length", &Vector3::length,
+		"length_squared", &Vector3::length_squared,
+		"normalize", &Vector3::normalize,
+		"normalized", &Vector3::normalized,
+		"is_normalized", &Vector3::is_normalized,
+		"inverse", &Vector3::inverse,
+		"limit_length", &Vector3::limit_length,
+		"zero", &Vector3::zero,
+		"snap", &Vector3::snap,
+		"snapped", &Vector3::snapped,
+		"rotate", &Vector3::rotate,
+		"rotated", &Vector3::rotated,
+		"lerp", &Vector3::lerp,
+		"slerp", &Vector3::slerp,
+		"cubic_interpolate", &Vector3::cubic_interpolate,
+		"cubic_interpolate_in_time", &Vector3::cubic_interpolate_in_time,
+		"bezier_interpolate", &Vector3::bezier_interpolate,
+		"move_toward", &Vector3::move_toward,
+		"octahedron_encode", &Vector3::octahedron_encode,
+		"octahedron_decode", &Vector3::octahedron_decode,
+		"octahedron_tangent_encode", &Vector3::octahedron_tangent_encode,
+		"octahedron_tangent_decode", &Vector3::octahedron_tangent_decode,
+		"cross", &Vector3::cross,
+		"dot", &Vector3::dot,
+		"outer", &Vector3::outer,
+		"abs", &Vector3::abs,
+		"floor", &Vector3::floor,
+		"sign", &Vector3::sign,
+		"ceil", &Vector3::ceil,
+		"round", &Vector3::round,
+		"clamp", &Vector3::clamp,
+		"distance_to", &Vector3::distance_to,
+		"distance_squared_to", &Vector3::distance_squared_to,
+		"posmod", &Vector3::posmod,
+		"posmodv", &Vector3::posmodv,
+		"project", &Vector3::project,
+		"angle_to", &Vector3::angle_to,
+		"signed_angle_to", &Vector3::signed_angle_to,
+		"direction_to", &Vector3::direction_to,
+		"slide", &Vector3::slide,
+		"bounce", &Vector3::bounce,
+		"reflect", &Vector3::reflect,
+		"is_equal_approx", &Vector3::is_equal_approx,
+		"is_zero_approx", &Vector3::is_zero_approx,
+		// cast methods
+		"to_string", &Vector3::operator String,
+		"to_vector3i", &Vector3::operator Vector3i,
+		// operators (logical operators are automatically bound by sol)
+		sol::meta_function::addition, &Vector3::operator +,
+		sol::meta_function::subtraction, sol::resolve<Vector3(const Vector3&) const>(&Vector3::operator -),
+		sol::meta_function::unary_minus, sol::resolve<Vector3() const>(&Vector3::operator -),
+		sol::meta_function::multiplication, sol::overload(
+			sol::resolve<Vector3(const Vector3&) const>(&Vector3::operator *),
+			sol::resolve<Vector3(const real_t) const>(&Vector3::operator *)
+		),
+		sol::meta_function::division, sol::overload(
+			sol::resolve<Vector3(const Vector3&) const>(&Vector3::operator /),
+			sol::resolve<Vector3(const real_t) const>(&Vector3::operator /)
+		),
+		sol::meta_function::to_string, &to_lua_string<Vector3>
 	);
 
 	return 0;
