1797987ab2f0b393384246ed201f2293a988bc59
gilzoide <gilzoide@gmail.com>
2023-09-24 15:44:13 -0300

    Add test for Variant methods

---
+++ test/lua_scripts/variant_method.lua
@@ -0,0 +1,4 @@
+local v = Vector2()
+assert(v:length(), "Method call without arguments failed")
+assert(v:angle_to(v) == 0, "Method call with valid arguments failed")
+assert(not pcall(v.angle_to, v, "Method call with invalid arguments succeeded"))
