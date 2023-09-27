b10de06e5cbe4f45be05a2fec09c489cdebd3dc5
gilzoide <gilzoide@gmail.com>
2023-09-27 08:12:48 -0300

    Change test_entrypoint to check for errors in GDScript
    When GDScript functions error, the function simply returns `null`, so now we use `true` to mark tests as successful

--- test/gdscript_tests/table_iterate.gd
+++ test/gdscript_tests/table_iterate.gd
@@ -2,11 +2,14 @@ extends RefCounted
 
 var lua_state = LuaState.new()
 
-func test():
+func test() -> bool:
+	var all_success = true
 	for i in range(0, 5):
-		test_n(i)
+		if not test_n(i):
+			all_success = false
+	return all_success
 
-func test_n(n: int):
+func test_n(n: int) -> bool:
 	var table = lua_state.create_table()
 	
 	for i in n:
@@ -16,3 +19,4 @@ func test_n(n: int):
 	for k in table:
 		iteration_count += 1
 	assert(iteration_count == n, "Iterated %d times over LuaTable with %d items" % [iteration_count, n])
+	return true
