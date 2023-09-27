27bbc56db13cf74430104dcd078273703d0bc933
gilzoide <gilzoide@gmail.com>
2023-09-27 08:48:39 -0300

    Add unit test about __tostring being called in LuaTable objects

---
+++ test/gdscript_tests/LuaTable_tostring.gd
@@ -0,0 +1,18 @@
+extends RefCounted
+
+
+func test():
+	var lua_state = LuaState.new()
+	lua_state.open_libraries()
+	var table = lua_state.do_string("""
+		function __tostring(self)
+			return "hello " .. (self.name or "world")
+		end
+		return setmetatable({}, { __tostring = __tostring })
+	""")
+	assert(not table is LuaError, "Lua code failed: %s" % table)
+	assert(str(table) == "hello world", "__tostring was not called")
+	table.set("name", "gilzoide")
+	assert(str(table) == "hello gilzoide", "__tostring was not called")
+	return true
+
