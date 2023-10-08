f39286b6b11e2edbabaa3c9855d66b59e72563d1
gilzoide <gilzoide@gmail.com>
2023-10-08 18:07:16 -0300

    Duplicate LuaCoroutine tests with coroutines returned from Lua

--- test/gdscript_tests/LuaCoroutine.gd
+++ test/gdscript_tests/LuaCoroutine.gd
@@ -17,14 +17,38 @@ func _init():
 	""")
 
 
+func _create_coroutine_lua():
+	return lua_state.do_string("""
+		return coroutine.create(function(value)
+			while value do
+				value = coroutine.yield(value)
+			end
+		end)
+	""")
+
+
 func test_create():
 	var coroutine = LuaCoroutine.create(yielding_function)
 	assert(coroutine is LuaCoroutine)
 	return true
 
 
+func test_create_lua():
+	var coroutine = _create_coroutine_lua()
+	assert(coroutine is LuaCoroutine)
+	return true
+
+
 func test_status():
 	var coroutine = LuaCoroutine.create(yielding_function)
+	assert(coroutine.status == LuaCoroutine.STATUS_YIELD)
+	coroutine.resume()
+	assert(coroutine.status == LuaCoroutine.STATUS_OK)
+	return true
+
+
+func test_status_lua():
+	var coroutine = _create_coroutine_lua()
 	assert(coroutine.status == LuaCoroutine.STATUS_YIELD)
 	coroutine.resume()
 	assert(coroutine.status == LuaCoroutine.STATUS_OK)
@@ -42,8 +66,30 @@ func test_send_and_receive_arguments():
 	return true
 
 
+func test_send_and_receive_arguments_lua():
+	var coroutine = _create_coroutine_lua()
+	for i in range(0, 10):
+		var result = coroutine.resume(i)
+		assert(result == i)
+	assert(coroutine.status == LuaCoroutine.STATUS_YIELD)
+	assert(coroutine.resume() == null)
+	assert(coroutine.status == LuaCoroutine.STATUS_OK)
+	return true
+
+
 func test_send_and_receive_array_arguments():
 	var coroutine = LuaCoroutine.create(yielding_function)
+	for i in range(0, 10):
+		var result = coroutine.resumev([i])
+		assert(result == i)
+	assert(coroutine.status == LuaCoroutine.STATUS_YIELD)
+	assert(coroutine.resumev([]) == null)
+	assert(coroutine.status == LuaCoroutine.STATUS_OK)
+	return true
+
+
+func test_send_and_receive_array_arguments_lua():
+	var coroutine = _create_coroutine_lua()
 	for i in range(0, 10):
 		var result = coroutine.resumev([i])
 		assert(result == i)
