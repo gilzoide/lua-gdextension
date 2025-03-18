41830d36806f4a1efa152e18bce4e1b8d18fe760
gilzoide <gilzoide@gmail.com>
2025-03-18 06:32:20 -0300

    Add error message to test_package_path_default and test_package_cpath_default tests

--- test/gdscript_tests/package_path.gd
+++ test/gdscript_tests/package_path.gd
@@ -34,13 +34,13 @@ func test_package_path_default() -> bool:
 	lua_state.package_path = ""
 	assert(lua_state.package_path == "")
 	lua_state.package_path = ";;"
-	assert(lua_state.package_path == initial_package_path)
+	assert(lua_state.package_path == initial_package_path, "Expected '%s', got '%s'" % [initial_package_path, lua_state.package_path])
 	lua_state.package_path = "prefix;;"
-	assert(lua_state.package_path == "prefix;" + initial_package_path)
+	assert(lua_state.package_path == "prefix;" + initial_package_path, "Expected '%s', got '%s'" % ["prefix;" + initial_package_path, lua_state.package_path])
 	lua_state.package_path = ";;suffix"
-	assert(lua_state.package_path == initial_package_path + ";suffix")
+	assert(lua_state.package_path == initial_package_path + ";suffix", "Expected '%s', got '%s'" % [initial_package_path + ";suffix", lua_state.package_path])
 	lua_state.package_path = "prefix;;suffix"
-	assert(lua_state.package_path == "prefix;" + initial_package_path + ";suffix")
+	assert(lua_state.package_path == "prefix;" + initial_package_path + ";suffix", "Expected '%s', got '%s'" % ["prefix;" + initial_package_path + ";suffix", lua_state.package_path])
 	return true
 
 
@@ -48,11 +48,11 @@ func test_package_cpath_default() -> bool:
 	lua_state.package_cpath = ""
 	assert(lua_state.package_cpath == "")
 	lua_state.package_cpath = ";;"
-	assert(lua_state.package_cpath == initial_package_cpath)
+	assert(lua_state.package_cpath == initial_package_cpath, "Expected '%s', got '%s'" % [initial_package_path, lua_state.package_path])
 	lua_state.package_cpath = "prefix;;"
-	assert(lua_state.package_cpath == "prefix;" + initial_package_cpath)
+	assert(lua_state.package_cpath == "prefix;" + initial_package_cpath, "Expected '%s', got '%s'" % ["prefix;" + initial_package_cpath, lua_state.package_path])
 	lua_state.package_cpath = ";;suffix"
-	assert(lua_state.package_cpath == initial_package_cpath + ";suffix")
+	assert(lua_state.package_cpath == initial_package_cpath + ";suffix", "Expected '%s', got '%s'" % [initial_package_cpath + ";suffix", lua_state.package_path])
 	lua_state.package_cpath = "prefix;;suffix"
-	assert(lua_state.package_cpath == "prefix;" + initial_package_cpath + ";suffix")
+	assert(lua_state.package_cpath == "prefix;" + initial_package_cpath + ";suffix", "Expected '%s', got '%s'" % ["prefix;" + initial_package_cpath + ";suffix", lua_state.package_path])
 	return true
