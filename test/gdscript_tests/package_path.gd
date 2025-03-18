8f557606c7df546a5529437bb4bc22e2f2928b16
gilzoide <gilzoide@gmail.com>
2025-03-18 08:34:01 -0300

    Normalize slashes on assertions of paths to avoid problems in Windows

--- test/gdscript_tests/package_path.gd
+++ test/gdscript_tests/package_path.gd
@@ -35,13 +35,13 @@ func test_package_path_default() -> bool:
 	lua_state.package_path = ""
 	assert(lua_state.package_path == "")
 	lua_state.package_path = ";;"
-	assert(lua_state.package_path == initial_package_path, "Expected '%s', got '%s'" % [initial_package_path, lua_state.package_path])
+	assert(lua_state.package_path.replace("\\", "/") == initial_package_path.replace("\\", "/"), "Expected '%s', got '%s'" % [initial_package_path.replace("\\", "/"), lua_state.package_path.replace("\\", "/")])
 	lua_state.package_path = "prefix;;"
-	assert(lua_state.package_path == "prefix;" + initial_package_path, "Expected '%s', got '%s'" % ["prefix;" + initial_package_path, lua_state.package_path])
+	assert(lua_state.package_path.replace("\\", "/") == "prefix;" + initial_package_path.replace("\\", "/"), "Expected '%s', got '%s'" % ["prefix;" + initial_package_path.replace("\\", "/"), lua_state.package_path.replace("\\", "/")])
 	lua_state.package_path = ";;suffix"
-	assert(lua_state.package_path == initial_package_path + ";suffix", "Expected '%s', got '%s'" % [initial_package_path + ";suffix", lua_state.package_path])
+	assert(lua_state.package_path.replace("\\", "/") == initial_package_path.replace("\\", "/") + ";suffix", "Expected '%s', got '%s'" % [initial_package_path.replace("\\", "/") + ";suffix", lua_state.package_path.replace("\\", "/")])
 	lua_state.package_path = "prefix;;suffix"
-	assert(lua_state.package_path == "prefix;" + initial_package_path + ";suffix", "Expected '%s', got '%s'" % ["prefix;" + initial_package_path + ";suffix", lua_state.package_path])
+	assert(lua_state.package_path.replace("\\", "/") == "prefix;" + initial_package_path.replace("\\", "/") + ";suffix", "Expected '%s', got '%s'" % ["prefix;" + initial_package_path.replace("\\", "/") + ";suffix", lua_state.package_path.replace("\\", "/")])
 	return true
 
 
@@ -49,11 +49,11 @@ func test_package_cpath_default() -> bool:
 	lua_state.package_cpath = ""
 	assert(lua_state.package_cpath == "")
 	lua_state.package_cpath = ";;"
-	assert(lua_state.package_cpath == initial_package_cpath, "Expected '%s', got '%s'" % [initial_package_path, lua_state.package_path])
+	assert(lua_state.package_cpath.replace("\\", "/") == initial_package_cpath.replace("\\", "/"), "Expected '%s', got '%s'" % [initial_package_path.replace("\\", "/"), lua_state.package_path.replace("\\", "/")])
 	lua_state.package_cpath = "prefix;;"
-	assert(lua_state.package_cpath == "prefix;" + initial_package_cpath, "Expected '%s', got '%s'" % ["prefix;" + initial_package_cpath, lua_state.package_path])
+	assert(lua_state.package_cpath.replace("\\", "/") == "prefix;" + initial_package_cpath.replace("\\", "/"), "Expected '%s', got '%s'" % ["prefix;" + initial_package_cpath.replace("\\", "/"), lua_state.package_path.replace("\\", "/")])
 	lua_state.package_cpath = ";;suffix"
-	assert(lua_state.package_cpath == initial_package_cpath + ";suffix", "Expected '%s', got '%s'" % [initial_package_cpath + ";suffix", lua_state.package_path])
+	assert(lua_state.package_cpath.replace("\\", "/") == initial_package_cpath.replace("\\", "/") + ";suffix", "Expected '%s', got '%s'" % [initial_package_cpath.replace("\\", "/") + ";suffix", lua_state.package_path.replace("\\", "/")])
 	lua_state.package_cpath = "prefix;;suffix"
-	assert(lua_state.package_cpath == "prefix;" + initial_package_cpath + ";suffix", "Expected '%s', got '%s'" % ["prefix;" + initial_package_cpath + ";suffix", lua_state.package_path])
+	assert(lua_state.package_cpath.replace("\\", "/") == "prefix;" + initial_package_cpath.replace("\\", "/") + ";suffix", "Expected '%s', got '%s'" % ["prefix;" + initial_package_cpath.replace("\\", "/") + ";suffix", lua_state.package_path.replace("\\", "/")])
 	return true
