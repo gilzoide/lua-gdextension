9a1abf45a9de93ff433396afe4e8df8b9f0e4258
gilzoide <gilzoide@gmail.com>
2025-05-31 12:29:49 -0300

    Update C# GDExtension Bindgen to 0.3.0

--- addons/csharp_gdextension_bindgen/csharp_gdextension_bindgen.gd
+++ addons/csharp_gdextension_bindgen/csharp_gdextension_bindgen.gd
@@ -23,6 +23,46 @@ const StringNameTypeName = {
 	StringNameType.METHOD_NAME: "MethodName",
 	StringNameType.SIGNAL_NAME: "SignalName",
 }
+const PASCAL_CASE_NAME_OVERRIDES = {
+	"BitMap": "Bitmap",
+	"JSONRPC": "JsonRpc",
+	"Object": "GodotObject",
+	"OpenXRIPBinding": "OpenXRIPBinding",
+	"SkeletonModification2DCCDIK": "SkeletonModification2DCcdik",
+	"SkeletonModification2DFABRIK": "SkeletonModification2DFabrik",
+	"SkeletonModification3DCCDIK": "SkeletonModification3DCcdik",
+	"SkeletonModification3DFABRIK": "SkeletonModification3DFabrik",
+	"System": "System_",
+	"Thread": "GodotThread",
+}
+const PASCAL_CASE_PART_OVERRIDES = {
+	"AA": "AA", # Anti Aliasing
+	"AO": "AO", # Ambient Occlusion
+	"FILENAME": "FileName",
+	"FADEIN": "FadeIn",
+	"FADEOUT": "FadeOut",
+	"FX": "FX",
+	"GI": "GI", # Global Illumination
+	"GZIP": "GZip",
+	"HBOX": "HBox", # Horizontal Box
+	"ID": "Id",
+	"IO": "IO", # Input/Output
+	"IP": "IP", # Internet Protocol
+	"IV": "IV", # Initialization Vector
+	"MACOS": "MacOS",
+	"NODEPATH": "NodePath",
+	"SPIRV": "SpirV",
+	"STDIN": "StdIn",
+	"STDOUT": "StdOut",
+	"USERNAME": "UserName",
+	"UV": "UV",
+	"UV2": "UV2",
+	"VBOX": "VBox", # Vertical Box
+	"WHITESPACE": "WhiteSpace",
+	"WM": "WM",
+	"XR": "XR",
+	"XRAPI": "XRApi",
+}
 
 
 func _enter_tree():
@@ -104,26 +144,12 @@ static func generate_csharp_script(
 	for enum_name in ClassDB.class_get_enum_list(cls_name, true):
 		enums.append(_generate_enum(cls_name, enum_name))
 
-	var inherited_enums = PackedStringArray()
-	if not parent_class_is_extension:
-		for inherited_class in _get_parent_classes(cls_name):
-			for enum_name in ClassDB.class_get_enum_list(inherited_class, true):
-				inherited_enums.append(_generate_enum(inherited_class, enum_name))
-
 	# INTEGER CONSTANTS
 	var integer_constants = PackedStringArray()
 	for constant_name in ClassDB.class_get_integer_constant_list(cls_name, true):
 		if not ClassDB.class_get_integer_constant_enum(cls_name, constant_name, true).is_empty():
 			continue
 		integer_constants.append(_generate_integer_constant(cls_name, constant_name))
-
-	var inherited_integer_constants = PackedStringArray()
-	if not parent_class_is_extension:
-		for inherited_class in _get_parent_classes(cls_name):
-			for constant_name in ClassDB.class_get_integer_constant_list(inherited_class, true):
-				if not ClassDB.class_get_integer_constant_enum(inherited_class, constant_name, true).is_empty():
-					continue
-				inherited_integer_constants.append(_generate_integer_constant(inherited_class, constant_name))
 
 	# PROPERTIES
 	var properties = PackedStringArray()
@@ -148,6 +174,8 @@ static func generate_csharp_script(
 	for method in ClassDB.class_get_method_list(cls_name, true):
 		if method["flags"] & (METHOD_FLAG_VIRTUAL | METHOD_FLAG_VIRTUAL_REQUIRED):
 			continue
+		if method["name"].begins_with("_"):
+			continue
 		method_names.append(method["name"])
 		methods.append(_generate_method(cls_name, method))
 
@@ -156,6 +184,8 @@ static func generate_csharp_script(
 		for inherited_class in _get_parent_classes(cls_name):
 			for method in ClassDB.class_get_method_list(inherited_class, true):
 				if method["flags"] & (METHOD_FLAG_VIRTUAL | METHOD_FLAG_VIRTUAL_REQUIRED):
+					continue
+				if method["name"].begins_with("_"):
 					continue
 				inherited_methods.append(_generate_method(inherited_class, method))
 
@@ -184,17 +214,9 @@ static func generate_csharp_script(
 		regions.append("#region Enums")
 		regions.append("\n\n".join(enums))
 		regions.append("#endregion")
-	if not inherited_enums.is_empty():
-		regions.append("#region Inherited Enums")
-		regions.append("\n\n".join(inherited_enums))
-		regions.append("#endregion")
 	if not integer_constants.is_empty():
 		regions.append("#region Integer Constants")
 		regions.append("\n\n".join(integer_constants))
-		regions.append("#endregion")
-	if not inherited_integer_constants.is_empty():
-		regions.append("#region Inherited Integer Constants")
-		regions.append("\n\n".join(inherited_integer_constants))
 		regions.append("#endregion")
 	if not properties.is_empty():
 		regions.append("#region Properties")
@@ -275,6 +297,11 @@ static func _generate_enum(cls_name: StringName, enum_name: StringName) -> Strin
 			common_prefix = constant_name
 		else:
 			common_prefix = _get_common_prefix(common_prefix, constant_name)
+	# Handle special case where one of the constants is present in all constant
+	# names: remove last word from prefix.
+	# Example case: Node.ProcessThreadMessages and FLAG_PROCESS_THREAD_MESSAGES
+	if common_prefix in ClassDB.class_get_enum_constants(cls_name, enum_name, true):
+		common_prefix = common_prefix.rsplit("_", false, 1)[0]
 
 	var constants = PackedStringArray()
 	for constant_name in ClassDB.class_get_enum_constants(cls_name, enum_name, true):
@@ -293,7 +320,7 @@ static func _generate_enum(cls_name: StringName, enum_name: StringName) -> Strin
 		flags = "[Flags]" if ClassDB.is_class_enum_bitfield(cls_name, enum_name) else "",
 		enum_name = enum_name,
 		constants = "\n".join(constants).indent("\t"),
-		maybe_enum_suffix = "" if enum_name.ends_with("Flags") else "Enum",
+		maybe_enum_suffix = "Enum" if _needs_enum_suffix(cls_name, enum_name) else "",
 	}).strip_edges()
 
 
@@ -307,7 +334,7 @@ static func _generate_integer_constant(cls_name: StringName, constant_name: Stri
 static func _generate_property(cls_name: StringName, property: Dictionary) -> String:
 	var property_name = property["name"]
 	var csharp_property_name = property_name.to_pascal_case()
-	var property_type = _get_property_type(property)
+	var property_type = _get_property_type(cls_name, property)
 
 	var getset = PackedStringArray()
 
@@ -315,7 +342,7 @@ static func _generate_property(cls_name: StringName, property: Dictionary) -> St
 	if getter:
 		if _is_extension_class(cls_name):
 			getset.append("get => {get_cast}_object.Get(PropertyName.{csharp_property_name});".format({
-				get_cast = _property_get_cast(property),
+				get_cast = _property_get_cast(cls_name, property),
 				csharp_property_name = csharp_property_name,
 			}))
 		else:
@@ -350,7 +377,7 @@ static func _generate_property(cls_name: StringName, property: Dictionary) -> St
 static func _generate_method(cls_name: StringName, method: Dictionary) -> String:
 	var method_name = method["name"]
 	var csharp_method_name = method_name.to_pascal_case()
-	var return_type = _get_method_return_type(method_name, method["return"])
+	var return_type = _get_method_return_type(cls_name, method_name, method["return"])
 	var is_static = method["flags"] & METHOD_FLAG_STATIC
 
 	var arg_types = PackedStringArray()
@@ -358,7 +385,7 @@ static func _generate_method(cls_name: StringName, method: Dictionary) -> String
 
 	var args = PackedStringArray()
 	for argument in method["args"]:
-		var arg_type = _get_property_type(argument)
+		var arg_type = _get_property_type(cls_name, argument)
 		var arg_name = "@" + argument["name"]
 		# hardcode type that cannot be known from reflection in GDScript
 		if method["name"] == "connect" and arg_name == "@flags":
@@ -379,13 +406,19 @@ static func _generate_method(cls_name: StringName, method: Dictionary) -> String
 	for default_value in default_args:
 		if default_value == null:
 			default_value = "default"
+		# handle enums
 		elif default_value is int and arg_types[i] != "int":
 			default_value = ("(%s)" % arg_types[i]) + str(default_value)
-		elif default_value is String:
-			default_value = '"%s"' % default_value
-		elif default_value is StringName:
+		# C# requires the "f" suffix for float literals
+		elif default_value is float and arg_types[i] == "float":
+			default_value = "%sf" % default_value
+		# NOTE: don't move this branch below the String one, since most of the
+		# time when arg_types[i] == StringName, default_value is String
+		elif default_value is StringName or arg_types[i] == "Godot.StringName":
 			implementation.append('%s ??= "%s";' % [arg_names[i], default_value])
 			default_value = "null"
+		elif default_value is String:
+			default_value = '"%s"' % default_value
 		elif default_value is Array:
 			assert(default_value.is_empty(), "Populated Array not supported yet! " + str(default_value)) # TODO: support populated array as default value
 			implementation.append("%s ??= new();" % arg_names[i])
@@ -393,6 +426,36 @@ static func _generate_method(cls_name: StringName, method: Dictionary) -> String
 		elif default_value is Dictionary:
 			assert(default_value.is_empty(), "Populated Dictionary not supported yet! " + str(default_value)) # TODO: support populated dictionary as default value
 			implementation.append("%s ??= new();" % arg_names[i])
+			default_value = "null"
+		elif (
+			default_value is Vector2 or default_value is Vector3 or default_value is Vector4
+			or default_value is Color
+		):
+			args[i] = args[i].replace(arg_types[i], arg_types[i] + "?")
+			var impl = "%s ??= new%s;" % [arg_names[i], default_value]
+			if not OS.has_feature("double"):
+				impl = impl.replace(",", "f,").replace(")", "f)")
+			implementation.append(impl)
+			default_value = "null"
+		elif (
+			default_value is PackedByteArray
+			or default_value is PackedInt32Array or default_value is PackedInt64Array
+			or default_value is PackedFloat32Array or default_value is PackedFloat64Array
+			or default_value is PackedVector2Array or default_value is PackedVector3Array or default_value is PackedVector4Array
+			or default_value is PackedColorArray
+		):
+			assert(default_value.is_empty(), "Populated Packed Array not supported yet! " + str(default_value))
+			implementation.append("%s ??= System.Array.Empty<%s>();" % [arg_names[i], arg_types[i].replace("[]", "")])
+			default_value = "null"
+		elif default_value is Transform2D:
+			assert(default_value == Transform2D.IDENTITY, "Only identity Transform2D is supported as default value")
+			args[i] = args[i].replace(arg_types[i], arg_types[i] + "?")
+			implementation.append("%s ??= Godot.Transform2D.Identity;" % arg_names[i])
+			default_value = "null"
+		elif default_value is Transform3D:
+			assert(default_value == Transform3D.IDENTITY, "Only identity Transform3D is supported as default value")
+			args[i] = args[i].replace(arg_types[i], arg_types[i] + "?")
+			implementation.append("%s ??= Godot.Transform3D.Identity;" % arg_names[i])
 			default_value = "null"
 		args[i] += " = " + str(default_value)
 		i += 1
@@ -408,12 +471,12 @@ static func _generate_method(cls_name: StringName, method: Dictionary) -> String
 		if is_static:
 			implementation.append("{maybe_return}ClassDB.ClassCallStatic(NativeName, {arg_names});".format({
 				arg_names = ", ".join(arg_names),
-				maybe_return = "return " + _property_get_cast(method["return"]) if return_type != "void" else "",
+				maybe_return = "return " + _property_get_cast(cls_name, method["return"]) if return_type != "void" else "",
 			}))
 		else:
 			implementation.append("{maybe_return}_object.Call({arg_names});".format({
 				arg_names = ", ".join(arg_names),
-				maybe_return = "return " + _property_get_cast(method["return"]) if return_type != "void" else "",
+				maybe_return = "return " + _property_get_cast(cls_name, method["return"]) if return_type != "void" else "",
 			}))
 	else:
 		if is_static:
@@ -448,11 +511,11 @@ static func _generate_method(cls_name: StringName, method: Dictionary) -> String
 static func _generate_signal(cls_name: StringName, sig: Dictionary):
 	var signal_name = sig["name"]
 	var csharp_signal_name = signal_name.to_pascal_case()
-	var return_type = _get_method_return_type(signal_name, sig["return"])
+	var return_type = _get_method_return_type(cls_name, signal_name, sig["return"])
 
 	var arg_types = PackedStringArray()
 	for argument in sig["args"]:
-		var arg_type = _get_property_type(argument)
+		var arg_type = _get_property_type(cls_name, argument)
 		arg_types.append(arg_type)
 
 	var delegate_type
@@ -491,7 +554,7 @@ static func _property_is_enum(property: Dictionary) -> bool:
 	return property["usage"] & (PROPERTY_USAGE_CLASS_IS_ENUM | PROPERTY_USAGE_CLASS_IS_BITFIELD)
 
 
-static func _get_property_type(property: Dictionary) -> String:
+static func _get_property_type(cls_name: StringName, property: Dictionary) -> String:
 	match property["type"]:
 		TYPE_NIL:
 			return "Variant"
@@ -502,8 +565,14 @@ static func _get_property_type(property: Dictionary) -> String:
 				var enum_name = property["class_name"]
 				if enum_name == "Error":
 					return "Godot.Error"
-				return enum_name + "Enum"
+				var split = enum_name.split(".")
+				if split.size() == 1:
+					return enum_name + ("Enum" if _needs_enum_suffix(cls_name, enum_name) else "")
+				else:
+					return enum_name + ("Enum" if _needs_enum_suffix(split[0], split[1]) else "")
 			return "int"
+		TYPE_FLOAT:
+			return "double" if OS.has_feature("double") else "float"
 		TYPE_STRING:
 			return "string"
 		TYPE_VECTOR2I:
@@ -520,7 +589,7 @@ static func _get_property_type(property: Dictionary) -> String:
 			return "Godot.Rid"
 		TYPE_OBJECT:
 			if property["class_name"] and property["class_name"] != "Object":
-				return property["class_name"]
+				return _pascal_to_pascal_case(_get_class_from_class_name(property["class_name"]))
 			else:
 				return "GodotObject"
 		TYPE_ARRAY:
@@ -557,18 +626,36 @@ static func _get_property_type(property: Dictionary) -> String:
 static func _get_mapped_variant_type(variant_type_name: String) -> String:
 	var _type_map = {
 		"Variant": "Variant",
-		"bool": "bool",
-		"int": "long",
-		"float": "double" if OS.has_feature("double") else "float",
-		"String": "string",
-		"StringName": "StringName",
-		"Dictionary": "Godot.Collections.Dictionary",
+		type_string(TYPE_BOOL): "bool",
+		type_string(TYPE_INT): "int",
+		type_string(TYPE_FLOAT): "double" if OS.has_feature("double") else "float",
+		type_string(TYPE_STRING): "string",
+		type_string(TYPE_STRING_NAME): "StringName",
+		type_string(TYPE_VECTOR2I): "Godot.Vector2I",
+		type_string(TYPE_RECT2I): "Godot.Rect2I",
+		type_string(TYPE_VECTOR3I): "Godot.Vector3I",
+		type_string(TYPE_VECTOR4I): "Godot.Vector4I",
+		type_string(TYPE_AABB): "Godot.Aabb",
+		type_string(TYPE_RID): "Godot.Rid",
+		type_string(TYPE_OBJECT): "GodotObject",
+		type_string(TYPE_ARRAY): "Godot.Collections.Array",
+		type_string(TYPE_DICTIONARY): "Godot.Collections.Dictionary",
+		type_string(TYPE_PACKED_BYTE_ARRAY): "byte[]",
+		type_string(TYPE_PACKED_INT32_ARRAY): "int[]",
+		type_string(TYPE_PACKED_INT64_ARRAY): "long[]",
+		type_string(TYPE_PACKED_FLOAT32_ARRAY): "float[]",
+		type_string(TYPE_PACKED_FLOAT64_ARRAY): "double[]",
+		type_string(TYPE_PACKED_STRING_ARRAY): "string[]",
+		type_string(TYPE_PACKED_VECTOR2_ARRAY): "Godot.Vector2[]",
+		type_string(TYPE_PACKED_VECTOR3_ARRAY): "Godot.Vector3[]",
+		type_string(TYPE_PACKED_VECTOR4_ARRAY): "Godot.Vector4[]",
+		type_string(TYPE_PACKED_COLOR_ARRAY): "Godot.Color[]",
 	}
-	return _type_map[variant_type_name]
+	return _type_map.get(variant_type_name, "Godot." + variant_type_name)
 
 
-static func _property_get_cast(property: Dictionary):
-	var property_type = _get_property_type(property)
+static func _property_get_cast(cls_name: StringName, property: Dictionary):
+	var property_type = _get_property_type(cls_name, property)
 	if _property_is_enum(property):
 		return "(%s)(int)" % property_type
 	else:
@@ -599,7 +686,7 @@ static func _first_non_extension_parent(cls_name: StringName) -> StringName:
 	return cls_name
 
 
-static func _get_method_return_type(method_name: StringName, method_return: Dictionary) -> String:
+static func _get_method_return_type(cls_name: StringName, method_name: StringName, method_return: Dictionary) -> String:
 	# hardcode type that cannot be known from reflection in GDScript
 	if method_name == "get_instance_id":
 		return "ulong"
@@ -610,7 +697,7 @@ static func _get_method_return_type(method_name: StringName, method_return: Dict
 		else:
 			return "void"
 	else:
-		return _get_property_type(method_return)
+		return _get_property_type(cls_name, method_return)
 
 
 static func _get_parent_classes(cls_name: StringName) -> Array[StringName]:
@@ -654,3 +741,109 @@ static func _get_common_prefix(s1: String, s2: String) -> String:
 		if s1[i] != s2[i]:
 			return s1.substr(0, i)
 	return s1.substr(0, common_length)
+
+
+static func _get_class_from_class_name(cls_name: String) -> String:
+	var classes = cls_name.split(",")
+	if classes.size() == 1:
+		return cls_name
+	
+	# Handle special case where 2 or more class names are present separated
+	# by ",": calculate the common parent class.
+	# Example case: CanvasItem.material uses "CanvasItemMaterial,ShaderMaterial"
+	var parent_classes = _get_parent_classes(classes[0])
+	for i in range(1, classes.size()):
+		var test_cls = classes[i]
+		while not ClassDB.is_parent_class(test_cls, parent_classes[0]):
+			parent_classes.pop_front()
+	return parent_classes[0]
+
+
+static func _needs_enum_suffix(cls_name: StringName, enum_name: String) -> bool:
+	var snake_case_enum_name = enum_name.to_snake_case()
+	if ClassDB.class_has_method(cls_name, snake_case_enum_name):
+		return true
+	if ClassDB.class_has_signal(cls_name, snake_case_enum_name):
+		return true
+	var properties = ClassDB.class_get_property_list(cls_name)
+	for property in properties:
+		if snake_case_enum_name == property["name"]:
+			return true
+	return false
+
+
+# Pascal case conversion used for class names.
+# Replicates the logic from `godot/modules/mono/utils/naming_utils.cpp`
+static func _is_ascii_upper_case(c: String) -> bool:
+	return c.to_upper() == c
+
+
+static func _is_ascii_lower_case(c: String) -> bool:
+	return c.to_lower() == c
+
+
+static func _is_digit(c: String) -> bool:
+	return c >= "0" and c <= "9"
+
+
+static func _split_pascal_case(p_identifier: String) -> PackedStringArray:
+	var parts := PackedStringArray()
+	var current_part_start := 0
+	var prev_was_upper := _is_ascii_upper_case(p_identifier[0])
+	for i in range(1, p_identifier.length()):
+		if prev_was_upper:
+			if _is_digit(p_identifier[i]) or _is_ascii_lower_case(p_identifier[i]):
+				if not _is_digit(p_identifier[i]):
+					# These conditions only apply when the separator is not a digit.
+					if i - current_part_start == 1:
+						# Upper character was only the beginning of a word.
+						prev_was_upper = false
+						continue
+					if i != p_identifier.length():
+						# If this is not the last character, the last uppercase
+						# character is the start of the next word.
+						i -= 1
+				if i - current_part_start > 0:
+					parts.append(p_identifier.substr(current_part_start, i - current_part_start))
+					current_part_start = i
+					prev_was_upper = false
+		else:
+			if _is_digit(p_identifier[i]) or _is_ascii_upper_case(p_identifier[i]):
+				parts.append(p_identifier.substr(current_part_start, i - current_part_start))
+				current_part_start = i
+				prev_was_upper = true
+
+	# Add the rest of the identifier as the last part.
+	if current_part_start != p_identifier.length():
+		parts.append(p_identifier.substr(current_part_start))
+	return parts
+
+
+static func _pascal_to_pascal_case(p_identifier: String) -> String:
+	if p_identifier.length() == 0:
+		return p_identifier
+	if p_identifier.length() <= 2:
+		return p_identifier.to_upper()
+	if PASCAL_CASE_NAME_OVERRIDES.has(p_identifier):
+		return PASCAL_CASE_NAME_OVERRIDES[p_identifier]
+	
+	var parts := _split_pascal_case(p_identifier)
+	var ret := ""
+	for part in parts:
+		if PASCAL_CASE_PART_OVERRIDES.has(part):
+			ret += PASCAL_CASE_PART_OVERRIDES[part]
+			continue
+		
+		if part.length() <= 2 and _is_ascii_upper_case(part):
+			ret += part.to_upper()
+			continue
+		
+		part[0] = part[0].to_upper()
+		for i in range(1, part.length()):
+			if _is_digit(part[i - 1]):
+				# Use uppercase after digits.
+				part[i] = part[i].to_upper()
+			else:
+				part[i] = part[i].to_lower()
+		ret += part
+	return ret
