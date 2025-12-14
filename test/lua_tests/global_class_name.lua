b00763c7098f9f9769a553bd5c311cc1ea53579f
gilzoide <gilzoide@gmail.com>
2025-12-14 19:54:18 -0300

    Add support for accessing named classes when `GODOT_CLASSES` is open

---
+++ test/lua_tests/global_class_name.lua
@@ -0,0 +1,4 @@
+assert(GlobalNodeScript, "Could not access GDScript from its class_name")
+assert(LuaBouncingLogo, "Could not access LuaScript from its class_name")
+assert(Variant.is(GlobalNodeScript, Script), "Global class is not a Script")
+assert(Variant.is(LuaBouncingLogo, Script), "Global class is not a Script")
