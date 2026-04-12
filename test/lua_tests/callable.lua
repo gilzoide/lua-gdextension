c908c654195b87adddad68201eeb3f0005bf4740
ivn-ln <illarn@ya.ru>
2026-04-13 00:44:37 +0300

    fixed callable test

--- test/lua_tests/callable.lua
+++ test/lua_tests/callable.lua
@@ -3,6 +3,10 @@ assert(callable() == OS:get_name())
 local a = 1
 local custom_callable = Callable(function(val)
 	a = a + val
-	print(a == 6 and "Passed" or "Failed")
+	return a
 end)
-custom_callable:call_deferred(5)
+local result = custom_callable(5)
+assert(a == 6 and result == 6)
+custom_callable:bind(1)
+custom_callable()
+assert(a == 7)
