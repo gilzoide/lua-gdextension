98b6b4ba3075325dcdc28dd45bac312cc31b455a
gilzoide <gilzoide@gmail.com>
2026-07-12 13:54:54 -0300

    Fix return bool from `test_entrypoint.gd::_process`

--- test/test_entrypoint.gd
+++ test/test_entrypoint.gd
@@ -3,7 +3,7 @@ extends SceneTree
 const LUA_TEST_DIR = "res://lua_tests"
 const GDSCRIPT_TEST_DIR = "res://gdscript_tests"
 
-func _process(_delta):
+func _process(_delta) -> bool:
 	var all_success = true
 
 	print("Starting Lua GDExtension tests (runtime: ", LuaState.get_lua_runtime(), ")")
@@ -46,3 +46,4 @@ func _process(_delta):
 			obj.queue_free()
 	
 	quit(0 if all_success else -1)
+	return true
